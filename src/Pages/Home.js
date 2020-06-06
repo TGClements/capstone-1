0e71167fa5bf6835deed0f33e7845c6324751264
Tyler Clements <tyler.g.clements@gmail.com>
2020-06-06 18:55:55 -0500

    Renamed images.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 function Home() {
-  return <div>This is placeholder for now.</div>;
+  return (
+    <div className='productContainer'>
+      <div className='card'>
+        <img src='1-webimg-watermark.png' className='card-img-top' alt='...' />
+        <div className='card-body'>
+          <h5 className='card-title'>Card Title</h5>
+          <p className='card-text'>This is some placeholder.</p>
+          <button className='btn btn-primary'>Add to Cart</button>
+        </div>
+      </div>
+      <div>This is even more placeholder.</div>
+    </div>
+  );
 }
 
 export default Home;
